15e5c6f7feee7dbb160322571a64cb277665e391
biel-igl <gabrielpires524@gmail.com>
2023-09-26 18:07:29 -0300

    formularios e relacionamentos de modelos

---
+++ eletiva-python/python/5-Django/dia-03/playlists/views.py
@@ -0,0 +1,42 @@
+# playlists/views.py
+
+from django.shortcuts import render, redirect
+from playlists.forms import CreateMusicModelForm, CreateSingerForm
+from playlists.models import Music, Singer
+
+
+def index(request):
+    context = {"musics": Music.objects.all()}
+    return render(request, "home.html", context)
+
+
+def create_music(request):
+    # form = CreateMusicForm()
+    form = CreateMusicModelForm()
+
+    if request.method == "POST":
+        # form = CreateMusicForm(request.POST)
+        form = CreateMusicModelForm(request.POST)
+
+        if form.is_valid():
+            Music.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+
+    context = {"form": form}
+
+    return render(request, "index.html", context)
+
+
+def singer(request):
+    form = CreateSingerForm()
+
+    if request.method == "POST":
+        form = CreateSingerForm(request.POST)
+
+        if form.is_valid():
+            Singer.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+
+    context = {"form": form}
+
+    return render(request, "singer.html", context)
